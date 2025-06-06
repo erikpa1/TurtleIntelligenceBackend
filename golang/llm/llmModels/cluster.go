4f2ed9ec659a1c8841b9451ff9ef74f14c95f9fa
Erik Palencik <erik.palencik@outlook.com>
2025-06-06 13:08:02 +0200

    LLM cluster udpate

--- golang/llm/llmModels/cluster.go
+++ golang/llm/llmModels/cluster.go
@@ -6,6 +6,6 @@ type LLMCluster struct {
 	Uid     primitive.ObjectID `json:"uid" bson:"_id"`
 	Name    string             `json:"name"`
 	Url     string             `json:"url"`
-	xApiKey string             `json:"xApiKey"`
+	xApiKey primitive.ObjectID `json:"xApiKey"`
 	Org     primitive.ObjectID `json:"org"`
 }
