2677f1a6f657504a44b48a67cc53e55384f9d6d0
Erik Palencik <erik.palencik@outlook.com>
2025-06-12 19:27:38 +0200

    Fixing cluster

--- golang/llm/llmModels/cluster.go
+++ golang/llm/llmModels/cluster.go
@@ -3,7 +3,7 @@ package llmModels
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type LLMCluster struct {
-	Uid     primitive.ObjectID `json:"uid" bson:"_id"`
+	Uid     primitive.ObjectID `json:"uid" bson:"_id,omitempty"`
 	Name    string             `json:"name"`
 	Url     string             `json:"url"`
 	xApiKey primitive.ObjectID `json:"xApiKey"`
