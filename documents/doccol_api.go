05a735ea49ddd4ee94aa24ca122841b41c204b26
Erik Palencik <erik.palencik@outlook.com>
2025-08-31 15:21:08 +0200

    Update documents intelligence

--- documents/doccol_api.go
+++ documents/doccol_api.go
@@ -1,7 +1,10 @@
 package documents
 
+import "C"
 import (
+	"fmt"
 	"github.com/erikpa1/TurtleIntelligenceBackend/auth"
+	"github.com/erikpa1/TurtleIntelligenceBackend/lg"
 	"github.com/erikpa1/TurtleIntelligenceBackend/tools"
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +13,16 @@ func _RefreshDocumentsCollection(c *gin.Context) {
 	user := auth.GetUserFromContext(c)
 	uid := tools.MongoObjectIdFromQuery(c)
 	coll := GetDocumentCollection(user, uid)
-	RefreshDocumentsCollection(c, user, coll)
+
+	lg.LogE(uid)
+
+	if coll == nil {
+		lg.LogE(fmt.Sprintf("Document collection [%s] not found", uid))
+		tools.AutoNotFound(c, "")
+	} else {
+		RefreshDocumentsCollection(c, user, coll)
+	}
+
 }
 
 func _CreateDocumentsCollection(c *gin.Context) {
@@ -24,15 +36,37 @@ func _ListDocumentCollections(c *gin.Context) {
 	tools.AutoReturn(c, ListDocumentCollections(user))
 }
 
+func _ListDocumentsOfCollection(c *gin.Context) {
+	user := auth.GetUserFromContext(c)
+	uid := tools.MongoObjectIdFromQuery(c)
+	tools.AutoReturn(c, ListDocumentsOfCollection(user, uid))
+}
+
 func _DeleteDocumentCollection(c *gin.Context) {
 	user := auth.GetUserFromContext(c)
 	uid := tools.MongoObjectIdFromQuery(c)
 	DeleteDocumentsCollection(user, uid)
 }
+
+func _UnassignDocFromCollection(c *gin.Context) {
+	user := auth.GetUserFromContext(c)
+	uid := tools.MongoObjectIdFromQuery(c)
+	UnassignDocumentsCollection(user, uid)
+}
+
+func _ClearCollectionDocuments(c *gin.Context) {
+	user := auth.GetUserFromContext(c)
+	uid := tools.MongoObjectIdFromQuery(c)
+	ClearDocumentsCollection(user, uid)
+}
+
 func InitDocumentCollectionsApi(r *gin.Engine) {
 	r.GET("/api/docs-cols", auth.LoginOrApp, _ListDocumentCollections)
+	r.GET("/api/docs-cols/docs", auth.LoginOrApp, _ListDocumentsOfCollection)
 	r.POST("/api/docs-col", auth.LoginOrApp, _CreateDocumentsCollection)
 	r.POST("/api/docs-col/refresh", auth.LoginOrApp, _RefreshDocumentsCollection)
 	r.DELETE("/api/docs-col", auth.LoginOrApp, _DeleteDocumentCollection)
+	r.DELETE("/api/docs-col/item", auth.LoginOrApp, _UnassignDocFromCollection)
+	r.DELETE("/api/docs-col/clear", auth.LoginOrApp, _ClearCollectionDocuments)
 
 }
--- documents/doccol_ctrl.go
+++ documents/doccol_ctrl.go
@@ -14,10 +14,60 @@ func ListDocumentCollections(user *models.User) []*DocumentsCollection {
 	return db.QueryEntities[DocumentsCollection](CT_DOC_COLLECTION, user.FillOrgQuery(nil))
 }
 
+func ListDocumentsOfCollection(user *models.User, colUid primitive.ObjectID) []*Document {
+
+	col := GetDocsCol(user, colUid)
+
+	if col != nil {
+
+		query := user.FillOrgQuery(bson.M{
+			"_id": bson.M{
+				"$in": col.Items,
+			},
+		})
+
+		result := db.QueryEntities[Document](CT_DOC, query)
+		return result
+	} else {
+		return make([]*Document, 0)
+	}
+
+}
+
+func GetDocsCol(user *models.User, colUid primitive.ObjectID) *DocumentsCollection {
+	query := user.FillOrgQuery(bson.M{
+		"_id": colUid,
+	})
+	tmp := db.QueryEntity[DocumentsCollection](CT_DOC_COLLECTION, query)
+	return tmp
+}
+
 func DeleteDocumentsCollection(user *models.User, uid primitive.ObjectID) {
 	db.DeleteEntity(CT_DOC_COLLECTION, user.FillOrgQuery(bson.M{
 		"_id": uid,
 	}))
+}
+
+func UnassignDocumentsCollection(user *models.User, uid primitive.ObjectID) {
+	db.UpdateOneCustom(CT_DOC_COLLECTION, user.FillOrgQuery(bson.M{
+		"_id": uid,
+	}),
+		bson.M{
+			"$pull": bson.M{
+				"items": uid, // the ObjectID you want to remove
+			}},
+	)
+}
+
+func ClearDocumentsCollection(user *models.User, uid primitive.ObjectID) {
+	db.UpdateOneCustom(CT_DOC_COLLECTION, user.FillOrgQuery(bson.M{
+		"_id": uid,
+	}),
+		bson.M{
+			"$set": bson.M{
+				"items": make([]primitive.ObjectID, 0),
+			}},
+	)
 }
 
 func GetDocumentCollection(user *models.User, id primitive.ObjectID) *DocumentsCollection {
@@ -37,14 +87,7 @@ func CreateDocumentsCollection(c *gin.Context, user *models.User, docColl *Docum
 
 func RefreshDocumentsCollection(c *gin.Context, user *models.User, docColl *DocumentsCollection) {
 
-	db.UpdateOneCustom(CT_DOC_COLLECTION, user.FillOrgQuery(bson.M{
-		"_id": docColl.Uid,
-	}),
-		bson.M{
-			"$set": bson.M{
-				"items": make([]primitive.ObjectID, 0),
-			}},
-	)
+	ClearDocumentsCollection(user, docColl.Uid)
 
 	for _, doc := range ListDocumentExtracts(user) {
 
