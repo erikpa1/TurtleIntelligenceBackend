6b31a8ce518bf65a26a10f57d1e5aea17ef166e6
Erik Palencik <erik.palencik@exe.sk>
2025-12-01 19:27:59 +0100

    Update brute force penetration

--- security/loginPenetration/loginPenetration_api.go
+++ security/loginPenetration/loginPenetration_api.go
@@ -1,10 +1,21 @@
 package loginPenetration
 
 import (
+	"github.com/erikpa1/TurtleIntelligenceBackend/auth"
 	"github.com/erikpa1/TurtleIntelligenceBackend/dynamicModules"
+	"github.com/erikpa1/TurtleIntelligenceBackend/tools"
 	"github.com/gin-gonic/gin"
 )
 
+func _RunPenetrationTesting(c *gin.Context) {
+	uid := tools.MongoObjectIdFromQuery(c)
+	RunLoginPenTest(uid)
+
+}
+
 func InitLoginPenetration(r *gin.Engine) {
+
+	r.POST("/api/security/login-pentesting", auth.LoginOrApp, _RunPenetrationTesting)
+
 	dynamicModules.InitDefaultEntitiesApi(r, "security", "login-penetration")
 }
---
+++ security/loginPenetration/loginPenetration_models.go
@@ -0,0 +1,29 @@
+package loginPenetration
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type BruteForceResult struct {
+	Success      bool
+	Password     string
+	Attempts     int64
+	Duration     time.Duration
+	StatusCode   int
+	ResponseBody string
+}
+
+type LoginPenetration struct {
+	Uid             primitive.ObjectID `json:"uid" bson:"_id,omitempty"`
+	Name            string             `json:"name"`
+	Url             string             `json:"url"`
+	Email           string             `json:"email"`
+	IterationsCount int64              `json:"iteratotionsCount" bson:"iterationsCount"`
+}
